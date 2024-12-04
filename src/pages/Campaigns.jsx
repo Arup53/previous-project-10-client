655248ffe28d78d222b46df646ed8336c4c01bcc
ARUP <arupkumarhowlader12@gmail.com>
2024-12-04 22:49:29 +0600

    sort functionality added in all campaigns

--- src/pages/Campaigns.jsx
+++ src/pages/Campaigns.jsx
@@ -1,15 +1,39 @@
 import { Link, useLoaderData } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ElementCard from "../components/ElementCard";
 import { TbFileDescription } from "react-icons/tb";
 
 function Campaigns() {
+  const [loading, setLoading] = useState(true);
   const data = useLoaderData();
   const [campaigns, setCampaigns] = useState(data);
   console.log(campaigns);
 
+  useEffect(() => {
+    setLoading(false);
+  }, [data]);
+
+  function handleSort() {
+    const sortedCampaigns = [...campaigns].sort(
+      (a, b) => b.minamount - a.minamount
+    );
+    console.log(sortedCampaigns);
+    setCampaigns(sortedCampaigns);
+  }
+
+  if (loading) {
+    return (
+      <div className="min-h-[600px] mx-auto flex justify-center items-center">
+        <span className="loading loading-bars loading-lg"></span>
+      </div>
+    );
+  }
+
   return (
     <div>
+      <button onClick={handleSort} className="btn">
+        Sort By Descending
+      </button>
       <div className="overflow-x-auto ">
         <table className="table">
           {/* head */}
@@ -20,6 +44,7 @@ function Campaigns() {
               <th>Cause(type)</th>
               <th>Added By</th>
               <th>Deadline</th>
+              <th>Minimum Amount</th>
               <th>Details</th>
             </tr>
           </thead>
@@ -33,6 +58,7 @@ function Campaigns() {
                   <td>{campaign.type}</td>
                   <td>{campaign.name}</td>
                   <td>{campaign.deadline}</td>
+                  <td>{campaign.minamount}</td>
                   <td>
                     <Link to={`/details/${campaign._id}`} className="btn">
                       <TbFileDescription />
