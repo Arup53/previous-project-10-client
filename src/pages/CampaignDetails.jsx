600fa0cb0bcbd4062958e1b3433be66433f8be62
ARUP <arupkumarhowlader12@gmail.com>
2024-12-04 18:42:37 +0600

    campaigns details UI struture modified

--- src/pages/CampaignDetails.jsx
+++ src/pages/CampaignDetails.jsx
@@ -15,6 +15,8 @@ function CampaignDetails() {
 
   console.log(date);
 
+  function handleDonateSubmission() {}
+
   if (loading) {
     return (
       <div className="min-h-[600px] mx-auto flex justify-center items-center">
@@ -29,7 +31,7 @@ function CampaignDetails() {
 
   return (
     <div className="my-24">
-      <div className="flex flex-col md:flex-row md:justify-between gap-12">
+      <div className="flex flex-col md:flex-row md:justify-between gap-6">
         {/* left */}
         <div className="flex flex-col gap-6 basis-4/5 shadow-2xl space-y-4 p-12">
           <figure>
@@ -37,14 +39,37 @@ function CampaignDetails() {
           </figure>
           <h3>{title}</h3>
           <p>{info}</p>
+          <button className="btn w-full">Donate</button>
         </div>
 
         {/* right */}
-        <div className="flex flex-col gap-6 basis-1/5  shadow-2xl space-y-4 px-3 py-6">
-          <h3></h3>
+        <div className="flex flex-col gap-6 basis-1/5  shadow-2xl space-y-4 px-2 py-6 h-[400px]">
+          <h3 className="text-center font-bold pt-4">About This Campaign</h3>
+          <div className="flex justify-between gap-1 text-xs ">
+            <p>Campaign Type</p>
+            <p>{type && type}</p>
+          </div>
           <div className="flex justify-between gap-1 text-xs ">
             <p>Deadline</p>
             <p>{date && date}</p>
+          </div>
+          <div className="flex justify-between gap-1 text-xs ">
+            <p>
+              Minimum Donation <br /> Amount
+            </p>
+            <p>{minamount && minamount}$</p>
+          </div>
+          <div className="flex justify-between gap-1 text-xs ">
+            <p>
+              Added by <br />
+            </p>
+            <p>{name && name}</p>
+          </div>
+          <div className="flex justify-between gap-1 text-xs ">
+            <p>
+              Email <br />
+            </p>
+            <p>{email && email}</p>
           </div>
         </div>
       </div>
