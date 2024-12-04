e12001a7855694ea5b3da4dd08799b25eeb9889a
ARUP <arupkumarhowlader12@gmail.com>
2024-12-04 20:39:51 +0600

    my campaign delete funtionality added

--- src/pages/MyCampaign.jsx
+++ src/pages/MyCampaign.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import { Navigate, useLoaderData } from "react-router-dom";
-import Swal from "sweetalert2";
 import { useAuthContext } from "../context/AuthProvider";
+import Swal from "sweetalert2";
+import { GrUpdate } from "react-icons/gr";
+import { MdOutlineDeleteForever } from "react-icons/md";
 
 function MyCampaign() {
   const { user, loading } = useAuthContext();
-  const [myCampaigns, setMyCampaigns] = useState(null);
+  const [myCampaigns, setMyCampaigns] = useState("");
 
   useEffect(() => {
     let { email } = user || {};
@@ -22,35 +24,38 @@ function MyCampaign() {
   }, [user]);
 
   console.log(myCampaigns);
-  // function handleDelete(id) {
-  //   Swal.fire({
-  //     title: "Are you sure?",
-  //     text: "You won't be able to revert this!",
-  //     icon: "warning",
-  //     showCancelButton: true,
-  //     confirmButtonColor: "#3085d6",
-  //     cancelButtonColor: "#d33",
-  //     confirmButtonText: "Yes, delete it!",
-  //   }).then((result) => {
-  //     if (result.isConfirmed) {
-  //       // delete from the database
-  //       fetch(`http://localhost:3000/users/${id}`, { method: "DELETE" })
-  //         .then((res) => res.json())
-  //         .then((data) => {
-  //           if (data.deletedCount) {
-  //             Swal.fire({
-  //               title: "Deleted!",
-  //               text: "Your file has been deleted.",
-  //               icon: "success",
-  //             });
-  //           }
-  //           const remaingUsers = users.filter((user) => user._id !== id);
-  //           setUsers(remaingUsers);
-  //           console.log(data);
-  //         });
-  //     }
-  //   });
-  // }
+
+  function handleDelete(id) {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // delete from the database
+        fetch(`http://localhost:3000/campaigns/${id}`, { method: "DELETE" })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success",
+              });
+            }
+            const remaingUsers = myCampaigns.filter(
+              (campaign) => campaign._id !== id
+            );
+            setMyCampaigns(remaingUsers);
+            console.log(data);
+          });
+      }
+    });
+  }
 
   if (loading) {
     return (
@@ -66,7 +71,49 @@ function MyCampaign() {
 
   return (
     <div className="my-24 py-12">
-      {myCampaigns && <div className="w-[80%] mx-auto "></div>}
+      <div className="w-[80%] mx-auto ">
+        {myCampaigns && (
+          <div className="overflow-x-auto">
+            <table className="table">
+              {/* head */}
+              <thead>
+                <tr className="space-x-2">
+                  <th></th>
+                  <th>Title</th>
+                  <th>Donation Type</th>
+                  <th>Minimum Dontation Amount</th>
+                  <th className="text-center">Deadline</th>
+                  <th className="text-center">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* row  */}
+                {myCampaigns &&
+                  myCampaigns?.map((campaign, idx) => (
+                    <tr key={campaign._id} className="hover">
+                      <th>{idx + 1}</th>
+                      <td>{campaign.title}</td>
+                      <td>{campaign.type}</td>
+                      <td>{campaign.minamount}</td>
+                      <td>{campaign.deadline}</td>
+                      <td className="flex gap-2">
+                        <button className="btn text-green-600  text-sm">
+                          <GrUpdate />
+                        </button>
+                        <button
+                          onClick={() => handleDelete(campaign._id)}
+                          className="btn text-red-600 text-sm"
+                        >
+                          <MdOutlineDeleteForever />
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
