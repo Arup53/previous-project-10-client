a8a05ce2693954495703b3b1403945ffd99abe81
ARUP <arupkumarhowlader12@gmail.com>
2024-12-07 18:44:36 +0600

    responsive done for login and signup page

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -54,7 +54,7 @@ function SignUp() {
     <div>
       <div className="h-[600px]  bg-[url('c-6.avif')]  bg-cover bg-no-repeat mb-24">
         <div className="h-full w-full bg-gradient-to-b from-black/40 to-black/60"></div>
-        <p className="bg-orange-400 text-4xl font-bold text-white text-center relative -top-14 left-1/3 px-12 py-6 w-[400px]">
+        <p className="bg-orange-400 text-lg md:text-4xl font-bold text-white text-center relative md:-top-14 -top-10 left-1/3 -translate-x-6 md:-translate-x-16 lg:translate-x-0 px-12 py-6 w-[200px] md:w-[400px]">
           Register
         </p>
       </div>
