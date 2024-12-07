cd37e8da538a6e950a7c864f6eb2dcfc9698b166
ARUP <arupkumarhowlader12@gmail.com>
2024-12-07 13:45:14 +0600

    background image path error fixed

--- src/pages/Campaigns.jsx
+++ src/pages/Campaigns.jsx
@@ -28,7 +28,7 @@ function Campaigns() {
 
   return (
     <div>
-      <div className="h-[600px] bg-[url('c-1.jpg')]  bg-cover bg-no-repeat mb-12">
+      <div className="h-[600px] bg-[url('/c-1.jpg')]  bg-cover bg-no-repeat mb-12">
         <div className="h-full w-full bg-gradient-to-b from-black/40 to-black/60"></div>
       </div>
       <p className="bg-orange-400 text-lg md:text-4xl font-bold text-white text-center relative -top-24  left-1/3 -translate-x-6 md:-translate-x-16 lg:translate-x-0 px-12 py-6 w-[200px] md:w-[400px] ">
